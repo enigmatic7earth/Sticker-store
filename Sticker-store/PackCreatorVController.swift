6e1f24c3de416636932266c58a035f058811b50e
NETBIZ <netbiz@netbizs-imac-2.local>
2019-02-28 18:06:56 +0530

    Added Info Window

--- Sticker-store/PackCreatorVController.swift
+++ Sticker-store/PackCreatorVController.swift
@@ -26,6 +26,22 @@ class PackCreatorVController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func guidlinesInfo(_ sender: Any){
+        
+        let infoWindow = UIAlertController(title: "Sticker guidelines", message: "To create your own sticker art, your stickers must meet the following requirements:\n\t• Each sticker is an image that has a transparent background.\n\t• Stickers must be exactly 512x512 pixels.\n\t• Each sticker must be less than 100 KB.\nYou must also provide an icon that will be used to represent your sticker pack in the WhatsApp sticker picker or tray. This image should be 96x96 pixels and must be less than 50 KB.", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        let more = UIAlertAction(title: "More", style: .default) { (more) in
+            // open web page: https://faq.whatsapp.com/general/26000226
+            if let url = URL(string: "https://faq.whatsapp.com/general/26000226") {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        infoWindow.addAction(ok)
+        infoWindow.addAction(more)
+        present(infoWindow, animated: true, completion: nil)
+ 
+        
+    }
     @IBAction func savePack(_ sender: Any) {
         
         
